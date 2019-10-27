29de01fcf9d0e55ce82e4357cd43fc9ac01461a8
Ashwin Bhat <me@ashwinbhat.com>
2019-10-27 10:56:10 -0700

    Conver to TypeScript (#2)
    * integrate with tsdx
    
    * adjust prettier settings
    
    * fix up tests

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import Boom from '@hapi/boom';
+
+type BoomFunction = <Data>(message?: string, data?: Data) => Boom.Boom<Data>;
+
+export default function(
+  condition: any,
+  errorMessage: string,
+  statusCodeOrBoomFunc: number | BoomFunction = 400,
+) {
+  if (!errorMessage) {
+    throw new Error('You must specify an error message.');
+  }
+
+  if (
+    !(
+      typeof statusCodeOrBoomFunc === 'number' ||
+      typeof statusCodeOrBoomFunc === 'function'
+    )
+  ) {
+    throw new Error('Third argument must be a number or function');
+  }
+
+  if (condition) {
+    return;
+  }
+
+  const sanitizedErrorMessage =
+    process.env.NODE_ENV === 'production' ? '' : errorMessage;
+  const error = new Error(sanitizedErrorMessage);
+
+  if (typeof statusCodeOrBoomFunc === 'function') {
+    throw statusCodeOrBoomFunc(sanitizedErrorMessage);
+  }
+
+  throw Boom.boomify(error, { statusCode: statusCodeOrBoomFunc });
+}
